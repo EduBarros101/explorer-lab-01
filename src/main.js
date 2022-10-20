901fb0a6e42ad3a9eed3d92d507bf25f178a8e9e
Eduardo Barros <edu.barros101@gmail.com>
2022-10-20 13:41:22 -0300

    Aula 03 concluída.

--- src/main.js
+++ src/main.js
@@ -73,10 +73,59 @@ const cardNumberPattern = {
       return number.match(item.regex)
     })
 
-    console.log(foundMask)
-
     return foundMask
   },
 }
 
 const cardNumberMasked = IMask(cardNumber, cardNumberPattern)
+
+const addButton = document.querySelector('#add-card')
+addButton.addEventListener('click', () => {
+  alert('Cartão adiconado.')
+})
+
+document.querySelector('form').addEventListener('submit', (e) => {
+  e.preventDefault()
+})
+
+const cardHolder = document.querySelector('#card-holder')
+
+cardHolder.addEventListener('input', () => {
+  const ccHolder = document.querySelector('.cc-holder .value')
+
+  ccHolder.innerText =
+    cardHolder.value.length === 0 ? 'Fulano da Silva' : cardHolder.value
+})
+
+securityCodeMasked.on('accept', () => {
+  updateSecutiryCode(securityCodeMasked.value)
+})
+
+function updateSecutiryCode(code) {
+  const ccSecurity = document.querySelector('.cc-security .value')
+
+  ccSecurity.innerText = code.length === 0 ? '123' : code
+}
+
+cardNumberMasked.on('accept', () => {
+  const cardType = cardNumberMasked.masked.currentMask.cardtype
+
+  setCardType(cardType)
+  updateCardNumber(cardNumberMasked.value)
+})
+
+function updateCardNumber(number) {
+  const ccNumber = document.querySelector('.cc-number')
+
+  ccNumber.innerText = number.length === 0 ? '1234 5678 9012 3456' : number
+}
+
+expirationDateMasked.on('accept', () => {
+  updateExpirationDate(expirationDateMasked.value)
+})
+
+function updateExpirationDate(date) {
+  const ccExpiration = document.querySelector('.cc-extra .value')
+
+  ccExpiration.innerText = date.length === 0 ? '02/32' : date
+}
